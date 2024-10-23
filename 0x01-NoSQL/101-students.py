24ea2faae0116327c09cd7b095c8c932e83abfa7
Ademitidayo <adenijibimmy08@gmail.com>
2024-10-23 05:09:15 +0100

    task 0

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+mod doc
+"""
+
+def top_students(mongo_collection):
+    """Returns all students sorted by their average score."""
+    pipeline = [
+        {
+            '$project': {
+                'name': 1,
+                'topics': 1,
+                'averageScore': { '$avg': '$topics.score' }
+            }
+        },
+        {
+            '$sort': { 'averageScore': -1 }
+        }
+    ]
+    
+    return list(mongo_collection.aggregate(pipeline))
