bed5b8b8fbb60f46904b4cb5f85160570e636164
Ademitidayo <adenijibimmy08@gmail.com>
2024-10-23 05:00:14 +0100

    NoSQL

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+mod doc
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    func doc
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
